df92e9e3a641a738594c594d4942f9dc10b6012d
faraz malik <farazmalik52@gmail.com>
2022-03-16 14:11:23 +0500

    added project setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const testRoute = require('./routes/test');
+
+
+const PORT=3000
+const app=express()
+
+app.use(express.json())
+app.use(testRoute)
+
+
+app.listen(PORT ,()=>
+{
+    console.log(`listening on Port ${PORT}`)
+})
